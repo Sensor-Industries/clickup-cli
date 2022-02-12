13a229ccfeefedc560fea8a7d4810684b5423428
Mat Taylor <mat@MacBook-Pro.local>
2022-02-11 23:20:35 -0800

    create, update and delete

--- README.md
+++ README.md
@@ -5,11 +5,13 @@ A Simple Command line Utility to create tasks, subtask and comments in clickup
 ### Install
 
 1. `> npm -i -g clickup-cli`
-2. Copy sample.js to ~/.clickup
+2. Copy sample.clickup to ~/.clickup
 3. Edit `~/.clickup` with default and auth token from your clickup account
 4. `> cu-cli help`
 
 ### Usage 
+
+```
 > cu-cli help
 
 Usage: cu-cli [options] [command]
@@ -28,12 +30,39 @@ Commands:
 
 ### Create Task
 ```
+> cu-cli help create
 
-Usage: clickup task [options] <name> [desc]
+Usage: cu-cli create [options] <name> [desc]
 
 create task
 
 Arguments:
+  name                            Task Name
+  desc                            Task Description
+
+Options:
+  -f, --file <filePath>           Markdown Description from file
+  -t, --parent <task_id>          Parent Task Id
+  -i, --priority <priority>       Task Priority (1-5)
+  -a, --assignees <user...>       Comma seperated user initals or ids
+  -e, --time_estimate <estimate>  Time Estimate (ms)
+  -s, --status <status>           Task Status
+  -p, --points <points>           Sprint Points
+  -j, --json <json>               Custom Fields as JSON
+  -l, --list <list...>            comma seperated lists names or ids
+  -h, --help                      display help for command  ```
+```
+
+### Update Task
+```
+> cu-cli help update
+
+Usage: cu-cli update [options] <task_id> [name] [desc]
+
+update task
+
+Arguments:
+  task_id                         Task Id
   name                            Task Name
   desc                            Task Description
 
@@ -45,13 +74,32 @@ Options:
   -e, --time_estimate <estimate>  Time Estimate (ms)
   -s, --status <status>           Task Status
   -p, --points <points>           Sprint Points
-  -c, --custom <json>             Custom Fields as JSON
+  -j, --json <json>               Custom Fields as JSON
   -l, --list <list...>            comma seperated lists names or ids
   -h, --help                      display help for command
-  ```
-  
-### Create Comment
 ```
+
+### Delete Task
+```
+> cu-cli help delete
+
+Usage: cu-cli delete [options] <task_id>
+
+delete task
+
+Arguments:
+  task_id     Task Id
+
+Options:
+  -h, --help  display help for command
+```
+
+
+### Add Comment
+
+```
+> cu-cli help comment
+
 Usage: clickup comment [options] <task_id> [message]
 
 add comment
@@ -66,8 +114,4 @@ Options:
   -a, --assignee <user_id>  Assign to user
   -h, --help                display help for command
 ```
-
-
-### Create Comment
-
 
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-//require.extensions['.clickup'] = require.extensions['.js']
+#! /usr/bin/env node
 
 import { Command } from 'commander'
 import clickup_api from 'clickup_api'
 import { promises as fs } from 'fs'
+import os from 'os'
 
 let config = {}
 let capi = null
@@ -10,15 +11,37 @@ const capp = new Command()
 
 capp.name('cu-cli').description('clickup cli')
 
-capp.option('-d, --debug').option('-c, --config', 'Custom Configuration File', '.clickup')
+capp.option('-d, --debug').option('-c, --config', 'Configuration File', os.homedir() + '/.clickup')
   .hook('preAction', async (cmd) => {
     config = JSON.parse(await fs.readFile(cmd.opts().config, 'utf8'))
     capi = new clickup_api(config.auth)
-    console.log(config)
+    if (cmd.opts().debug) console.log(config)
   })
 
-capp.command('task').description('create task')
+capp.command('create').description('create task')
   .argument('<name>', 'Task Name')
+  .argument('[desc]', 'Task Description')
+  .option('-f, --file <filePath>', 'Markdown Description from file')
+  .option('-t, --parent <task_id>', 'Parent Task Id')
+  .option('-i, --priority <priority>', 'Task Priority (1-5)')
+  .option('-a, --assignees <user...>', 'Comma seperated user initals or ids')
+  .option('-e, --time_estimate <estimate>', 'Time Estimate (ms)')
+  .option('-s, --status <status>', 'Task Status')
+  .option('-p, --points <points>', 'Sprint Points')
+  .option('-j, --json <json>', 'Custom Fields as JSON')
+  .option('-l, --list <list...>', 'comma seperated lists names or ids')
+  .action(async (name, desc, opts) => {
+    try {
+      let data = Object.assign(config.defaults, opts, { name: name, content: desc})
+      if (opts.file) data.markdown_description = await fs.readFile(opts.file, 'utf8')
+      let res = await capi.Tasks.create_task(opts.list || config.defaults.list, data)
+      return opts.debug ? res : null
+    } catch (e) { return e.toString() }
+  })
+
+capp.command('update').description('update task')
+  .argument('<task_id>', 'Task Id')
+  .argument('[name]', 'Task Name')
   .argument('[desc]', 'Task Description')
   .option('-f, --file <filePath>', 'Markdown Description from file')
   .option('-t, --parent <task_id>', 'Parent Task Id')
@@ -27,18 +50,22 @@ capp.command('task').description('create task')
   .option('-e, --time_estimate <estimate>', 'Time Estimate (ms)')
   .option('-s, --status <status>', 'Task Status')
   .option('-p, --points <points>', 'Sprint Points')
-  .option('-j, --custom <json>', 'Custom Fields as JSON')
+  .option('-j, --json <json>', 'Custom Fields as JSON')
   .option('-l, --list <list...>', 'comma seperated lists names or ids')
-  .action(async (name, desc, opts) => {
+  .action(async (tid, name, desc, opts) => {
     try {
-      console.log("here1", opts)
-      console.log("here2", config)
-      let capi = new clickup_api(config.auth)
-      //let data = merge(opts, { name: name, content: desc})
       let data = Object.assign(config.defaults, opts, { name: name, content: desc})
       if (opts.file) data.markdown_description = await fs.readFile(opts.file, 'utf8')
-      console.log('TASK:', data)
-      return await capi.Tasks.create_task(opts.list || config.defaults.list, data)
+      let res = await capi.Tasks.update_task(task_id, data)
+      return opts.debug ? res : null
+    } catch (e) { return e.toString() }
+  })
+
+capp.command('delete').description('delete task')
+  .argument('<task_id>', 'Task Id')
+  .action(async (tid, opts) => {
+    try {
+      return await capi.Tasks.delete_task(tid)
     } catch (e) { return e.toString() }
   })
 
@@ -53,17 +80,19 @@ capp.command('comment')
     try {
       let data = Object.assign(opts, { comment_text: msg })
       if (opts.file) data.comment_text = await fs.readFile(opts.file, 'utf8')
-      return await capi.Comments.create_task_comment(tid, data) 
+      let res = await capi.Comments.create_task_comment(tid, data)
+      return opts.debug ? res : null
+    
     } catch(e) { return e.toString() }
   })
 
 capp.parse()
 
 function merge(opts, extra={}) {
-  if (opts.config) config = require(opts.config)  
+  if (opts.config) config = require(opts.config)
   opts.assignees ??= opts.assignees.map(_ => config.users[_] || _)
   opts.lists ??= opts.lists.map(a => config.lists[_] || _)
-  return opts.custom
-    ? Object.assign(config.defaults, opts, JSON.parse(opts.custom), extra)
+  return opts.json
+    ? Object.assign(config.defaults, opts, JSON.parse(opts.json), extra)
     : Object.assign(config.defaults, opts, extra)
 }
